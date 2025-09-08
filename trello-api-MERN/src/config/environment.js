ea8789463ccfb8e4e228f2fd99a719f011375960
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2025-09-08 12:57:13 +0700

    set up environment

--- trello-api-MERN/src/config/environment.js
+++ trello-api-MERN/src/config/environment.js
@@ -8,7 +8,11 @@ dotenv.config(path.join(__dirname, '/..', '/.env'))
 export const env = {
   LOCAL_DEV_APP_PORT: process.env.LOCAL_DEV_APP_PORT,
   LOCAL_DEV_APP_HOST: process.env.LOCAL_DEV_APP_HOST,
+
   MONGO_URI: process.env.MONGO_URI,
   DB_NAME: process.env.DB_NAME,
+
+  BUILD_MODE: process.env.BUILD_MODE,
+
   AUTHOR: process.env.AUTHOR
 }
