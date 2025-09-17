55618c5ef82dce5383b187862808ba53f6b334bd
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2025-09-17 12:11:34 +0700

    create_basic_layout

--- vite-trello-web-base-project-master/src/App.jsx
+++ vite-trello-web-base-project-master/src/App.jsx
@@ -1,29 +1,22 @@
-import { useColorScheme } from '@mui/material/styles'
-import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
 import DarkLightToggle from './components/DarkLightToggle/DarkLightToggle'
-
-function ModeToggle() {
-
-  const { mode, setMode } = useColorScheme()
-
-
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
+import Box from '@mui/material/Box'
 
 function App() {
 
   return (
     <>
-      <ModeToggle/>
-      <DarkLightToggle />
+      <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+        <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', height: (theme) => `${ theme.trello.appBardHeight }`}}>
+          <DarkLightToggle />
+        </Box>
+        <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', height: (theme) => `${ theme.trello.boardBarHeight }` }}>
+          Board Bar
+        </Box>
+        <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', height: (theme) => `calc(100vh - ${theme.trello.appBardHeight} - ${theme.trello.boardBarHeight })` }}>
+          Board Content
+        </Box>
+      </Container>
     </>
   )
 }
